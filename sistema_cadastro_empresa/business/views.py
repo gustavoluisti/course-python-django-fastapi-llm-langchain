6fa95e4f04c78ed548ff96baccd74f9c430e2a09
Luis Gustavo <gustavoluisrh@gmail.com>
2026-07-03 13:49:49 -0300

    feat: add business

--- sistema_cadastro_empresa/business/views.py
+++ sistema_cadastro_empresa/business/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from rest_framework import generics
+from business.models import Business
+from business.serializers import BusinessSerializer
 
-# Create your views here.
+class BusinessCreateListView(generics.ListCreateAPIView):
+    queryset = Business.objects.all()
+    serializer_class = BusinessSerializer
