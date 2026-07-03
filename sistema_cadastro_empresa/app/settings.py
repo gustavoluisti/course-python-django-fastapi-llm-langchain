5bc6d6d034c2086f0768ef4a4651d3b2ccc5fc97
Luis Gustavo <gustavoluisrh@gmail.com>
2026-07-03 13:11:59 -0300

    feat: add model

--- sistema_cadastro_empresa/app/settings.py
+++ sistema_cadastro_empresa/app/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+
+    "rest_framework",
+
+    "business"
 ]
 
 MIDDLEWARE = [
@@ -103,7 +107,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
-LANGUAGE_CODE = "en-us"
+LANGUAGE_CODE = "pt-br"
 
 TIME_ZONE = "UTC"
 
