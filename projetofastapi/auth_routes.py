034b690144c6504ca75237086eeda362fe84cc1d
Luis Gustavo <gustavol>
2026-07-15 21:31:57 -0300

    feat: add user

--- projetofastapi/auth_routes.py
+++ projetofastapi/auth_routes.py
@@ -1,7 +1,21 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from models import Usuario, db
+from dependencies import pegar_sessao
 
 auth_router = APIRouter(prefix="/auth", tags=["auth"])
 
 @auth_router.get("/")
 async def autenticar():
     return {"mensagem": "Você acessou a rota padrão de autenticação", "autenticado": False}
+
+@auth_router.post("/criar_conta")
+async def criar_conta(email: str, senha:str, nome: str, session = Depends(pegar_sessao)):
+    usuario = session.query(Usuario).filter(Usuario.email==email).first()
+    if usuario:
+        # Ja existe um usuario com esse email
+        return {"mensagem": "Já existe um usuário com esse email"}
+    else: 
+        novo_usuario = Usuario(nome, email, senha)
+        session.add(novo_usuario)
+        session.commit()
+        return { "mensagem": "Usuário cadastrado com sucesso"}
